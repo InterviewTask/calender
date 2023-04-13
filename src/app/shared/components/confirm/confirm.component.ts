ccfdb12047bbfba8c79558830a3fb065cea419f6
Ali Hakimi <ali.hakimi.njf@gmail.com>
2023-04-13 18:42:24 +0430

    fix: comfirm button

--- src/app/shared/components/confirm/confirm.component.ts
+++ src/app/shared/components/confirm/confirm.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirm',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./confirm.component.scss']
 })
 export class ConfirmComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: {
+      title: string,
+      message: string,
+      button: string,
+      action?: string,
+      classes?: string
+    },
+  ) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {}
+  close() {
+    this.dialogRef.close();
   }
-
+  submit() {
+    if (this.data.action === 'clipboard') {
+      const textarea = document.createElement('textarea');
+      textarea.textContent = this.data.message;
+      document.body.appendChild(textarea);
+      textarea.select();
+      try {
+        document.execCommand('copy');
+      } catch (error) {
+        console.log(error);
+      }
+      document.body.removeChild(textarea);
+      setTimeout(() => {
+      }, 3000);
+      return;
+    }
+    this.dialogRef.close(true);
+  }
 }
