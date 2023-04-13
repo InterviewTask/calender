f8e8a4f5a6b8e3e40e6c6acd2f5440ef63cb4277
Ali Hakimi <ali.hakimi.njf@gmail.com>
2023-04-13 12:03:20 +0430

    feat: create confirm component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ConfirmComponent } from './components/confirm/confirm.component';
 
+const COMPONENTS = [
+  ConfirmComponent
+]
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ...COMPONENTS
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    ...COMPONENTS
   ]
 })
 export class SharedModule { }
